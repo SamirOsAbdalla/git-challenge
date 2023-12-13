6c2c342f9c870cbafc3eda3e22f8bfc6b9369dc2
samir01 <samir.os.abdalla@gmail.com>
2023-12-13 11:58:24 -0800

    created initial outline for reading a git object using its sha

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,7 +2,10 @@
 #include <filesystem>
 #include <fstream>
 #include <string>
+#include <zlib.h>
 
+#define BUFFER_SIZE 1024
+#define DECOMPRESSED_BUFFER_SIZE 1024
 int main(int argc, char *argv[])
 {
 
@@ -41,6 +44,35 @@ int main(int argc, char *argv[])
         catch (const std::filesystem::filesystem_error &e)
         {
             std::cerr << e.what() << '\n';
+            return EXIT_FAILURE;
+        }
+    }
+    else if (command == "cat-file" && argc == 4)
+    {
+        std::string flag = argv[2];
+        if (flag == "-p")
+        {
+            const std::string sha = argv[3];
+            const std::string path = ".git/objects/" + sha;
+
+            std::ofstream file(path, std::ios::binary);
+            unsigned char buffer[BUFFER_SIZE];
+
+            file << buffer;
+            unsigned char decompressed_buffer[DECOMPRESSED_BUFFER_SIZE];
+            uLongf decompressed_buffer_size = DECOMPRESSED_BUFFER_SIZE;
+            int result = uncompress(decompressed_buffer, &decompressed_buffer_size, buffer, BUFFER_SIZE);
+            if (result != Z_OK)
+            {
+                std::cerr << "Uncompression Error\n";
+                return EXIT_FAILURE;
+            }
+
+            std::cout.write(reinterpret_cast<const char *>(decompressed_buffer), decompressed_buffer_size);
+        }
+        else
+        {
+            std::cerr << "Unknown command " << command << '\n';
             return EXIT_FAILURE;
         }
     }
