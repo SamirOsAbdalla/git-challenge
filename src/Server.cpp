31e3be6fff32d083f60f9d213f2334f512e48ff0
samir01 <samir.os.abdalla@gmail.com>
2024-01-05 11:37:06 -0800

    removed initialization of destination buffer size

--- src/Server.cpp
+++ src/Server.cpp
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
 
         std::vector<unsigned char> buffer(std::istreambuf_iterator<char>(inputStream), {});
 
-        uLongf decompressedBufferSize = buffer.size() * 10;
+        uLongf decompressedBufferSize;
         std::vector<unsigned char> decompressedBuffer(buffer.size() * 10);
 
         int result = uncompress((Bytef *)decompressedBuffer.data(), &decompressedBufferSize, (Bytef *)buffer.data(), BUFFER_SIZE);
